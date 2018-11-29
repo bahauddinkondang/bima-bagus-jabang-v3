7a64efde707556b7346f1f6f5ea4199258a38bb7
cyberghost69 <45302841+cyberghost69@users.noreply.github.com>
2018-11-29 16:34:06 +0700

    Add files via upload

---
+++ bima-bagus-jabang-v3.sh
@@ -0,0 +1,165 @@
+#Mau apa tolol??
+#recode?? Pm gw bangsat!!!!
+#gpp untuk niatnya cuman belajar
+blue='\033[34;1m'
+green='\033[32;1m'
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'
+yellow='\033[33;1m'
+clear
+toilet -f big -F gay "USER"
+echo ""
+echo $green"MASUKAN USERNAME DENGAN BENAR"
+echo $red"KALAU TIDAK TAHU USERNAMENYA HUB GW"
+echo $yellow"Whatsapp : 085695570951"
+echo ""
+echo $cyan"======================<USERNAME>======================"
+read -p"Masukan Username: " us
+if [ $us = "bimab-jabang" ];then
+sleep 1
+clear
+else
+clear
+toilet -f big -F gay "Salah"
+toilet -f big -F gay "Goblok"
+sleep 1
+clear
+sh pw+us.sh
+fi
+
+toilet -f big -F gay "PASWRD"
+echo ""
+echo $cyan"MASUKAN PASSWRD DGN BENAR"
+echo $purple"KALAU GK TAHU PASSWORDNYA HUB GW"
+echo $yellow"Whastapp : 085695570951"
+echo ""
+echo $green"======================PASSWORDNYA======================"
+read -p"Masukan Password: " pw
+if [ $pw = "jabang" ];then
+sleep 1
+clear
+else
+clear
+toilet -f big -F gay "Salah"
+toilet -f big -F gay "Goblok"
+sleep 1
+clear
+sh pw+us.sh
+fi
+
+toilet -f big -F gay "T00LS"
+toilet -f big -F gay "Bima Bagus"
+toilet -f big -F gay "V : 3"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"<[+]>Pembuat : cyb3r_gh0st" /$yellow"Bima Bagus T"
+echo $purple"<[+]>Team : D35TR0Y" $red"SQUAD"
+echo $green"<[+]>Alamat : INDONESIA "$blue"JAWA TIMUR"
+echo $red"<[+]>Whast'app : "$yellow"085695570951"
+echo $white"<[+]>Status : Smp-9"$purple "14Thn"$green "Jomblo Pacarin Aja"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo ""
+echo ""
+echo ""
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(1)SPECIAL THANKS FOR:"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(2)INSTALL TOOLS Cyb3r_Gh0st Versi:1"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(3)INSTALL TOOLS Cyb3r_Gh0st Versi:2"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(4)INSTALL TOOLS Cyb3r_Gh0st Versi:3"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(5)INSTALL TOOLS TYREXUS Versi:1"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(6)INSTALL TOOLS KUBURAN MANTAN cyb3r_gh0st"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo $cyan"(88)KELUAR"
+echo $yellow"<[+]>====="$green"====="$purple"====="$red"====="$cyan"====="$blue"====="$white"====="$cyan"====="$red"=======>"
+echo ""
+read -p"Pilih No===>>: " bos
+
+if [ $bos = 1 ] || [ $bos = 1 ]
+then
+clear
+toilet -f big -F gay "Loading"
+echo "Cyb3r_Gh0st"|lolcat
+echo "M.A"|lolcat
+echo "Tyrexus"|lolcat
+echo "Mr.Explore57"|lolcat
+echo "25 jul|666 ghost"|lolcat
+echo "Adnstro"|lolcat
+echo "Mr.spoon"|lolcat
+echo "WANKONDEX"|lolcat
+echo "Raka"|lolcat
+echo "Safri"|lolcat
+echo "ALL MEMBER/ADMIN D35TR0Y SQUAD"|lolcat
+fi
+
+if [ $bos = 2 ] || [ $bos = 2 ]
+then
+clear
+toilet -f big -F gay "Menginstall..."
+git clone https://github.com/intect15/bima-jabang
+cd bima-jabang
+sh BIMA-JABANG#1.sh
+fi
+
+if [ $bos = 3 ] || [ $bos = 3 ]
+then
+clear
+toilet -f big -F gay "Menginstall..."
+git clone https://github.com/intect15/bimajabangversi2
+cd bimajabangversi2
+sh cyb3r_gh0stv2.sh
+fi
+
+if [ $bos = 4 ] || [ $bos = 4 ]
+then
+clear
+toilet -f big -F gay "Menginstall..."
+figlet dasar|lolcat
+figlet goblok!!|lolcat
+echo $cyan"Inilah tools versi 3 !!!!"
+fi
+
+if [ $bos = 5 ] || [ $bos = 5 ]
+then
+clear
+toilet -f big -F gay "Menginstall..."
+git clone https://github.com/cyberghost69/tyrexusV1
+cd tyrexusV1
+sh destroy.sh
+fi
+
+if [ $bos = 6 ] || [ $bos = 6 ]
+then
+clear
+toilet -f big -F gay "Menginstall..."
+git clone https://github.com/cyberghost69/KUBURAN-MANTAN
+cd KUBURAN-MANTAN
+sh kuburan-mantan.sh
+fi
+
+if [ $bos = 88 ] || [ $bos = 88 ]
+then
+clear
+figlet yakin?|lolcat
+read -p"Yakin ingin keluar? (y/t)=> " bos;
+fi
+
+if [ $bos = y ] || [ $bos = y ]
+then
+clear
+figlet Keluar|lolcat
+exit
+fi
+
+if [ $bos = t ] || [ $bos = t ]
+then
+clear
+figlet Auto|lolcat
+figlet Login|lolcat
+sh bima-bagus-jabang-v3.sh
+fi
